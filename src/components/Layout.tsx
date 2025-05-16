415c29fd609e893fa88f50f86e16407939ad09c5
Peggy <peggychou0610@gmail.com>
2025-05-16 15:24:12 +0800

    feat: set min-height to layout component
    Set min-height to layout component to avoid child components from shrinking too small.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import PreviewArea from './PreviewArea';
 
 const Layout: React.FC = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen min-h-[500px] overflow-y-auto">
       {/* Left side: Editing Area */}
       <div className="w-1/2 h-full p-4 border-r border-gray-300">
         <EditingArea />
